d618acdf2484d5c859cefc009d1ef2d1e890fa59
Lucas Guzman <lucas_guzman@hotmail.es>
2025-04-04 16:58:13 +0200

    Añadir los triggers y tests de aguado

--- triggers.sql
+++ triggers.sql
@@ -1,3 +1,5 @@
+-- Prevent institutional users from creating posts trigger
+
 CREATE OR REPLACE TRIGGER prevent_posts_institutional_trigger
 BEFORE INSERT OR UPDATE ON posts
 FOR EACH ROW
@@ -14,5 +16,35 @@ BEGIN
     IF v_user_type = 'L' THEN
         RAISE_APPLICATION_ERROR(-20050, 'Institutional users (libraries) cannot create posts.');
     END IF;
+END;
+/
+
+-- Derregistration book trigger
+
+CREATE OR REPLACE TRIGGER deregistration_book
+BEFORE UPDATE ON COPIES
+FOR EACH ROW
+BEGIN
+  IF :NEW.condition = 'D' THEN
+    IF :OLD.deregistered IS NOT NULL THEN
+      RAISE_APPLICATION_ERROR(-20001, 'Copy has already been deregistered.');
+    ELSE
+      :NEW.deregistered := SYSDATE;
+    END IF;
+  END IF;
+END;
+/
+
+-- Update book reads trigger
+
+ALTER TABLE books ADD reads NUMBER DEFAULT 0;
+
+CREATE OR REPLACE TRIGGER update_book_reads
+AFTER INSERT ON loans
+FOR EACH ROW
+BEGIN
+    UPDATE books
+    SET reads = reads + 1
+    WHERE title = (SELECT title FROM editions WHERE isbn = (SELECT isbn FROM copies WHERE signature = :NEW.signature));
 END;
 /
--- triggers_test.sql
+++ triggers_test.sql
@@ -13,3 +13,65 @@ EXCEPTION
         ROLLBACK;
 END;
 /
+
+
+SELECT b.TITLE, b.AUTHOR
+FROM books b
+JOIN editions e ON b.TITLE = e.TITLE AND b.AUTHOR = e.AUTHOR
+JOIN copies c ON e.ISBN = c.ISBN
+LEFT JOIN loans l ON c.SIGNATURE = l.SIGNATURE
+GROUP BY b.TITLE, b.AUTHOR
+HAVING COUNT(DISTINCT e.LANGUAGE) >= 3
+   AND COUNT(l.SIGNATURE) = 0;
+
+
+UPDATE editions 
+SET language = 'gallego' 
+WHERE isbn = '978-84-683-5157-5' 
+AND title = 'Ben'; 
+
+SELECT d.fullname,
+       FLOOR(MONTHS_BETWEEN(SYSDATE, d.birthdate) / 12) AS age,
+       FLOOR(MONTHS_BETWEEN(SYSDATE, d.cont_start) / 12) AS seniority,
+       COUNT(DISTINCT EXTRACT(YEAR FROM a.taskdate)) AS active_years,
+       COUNT(DISTINCT s.town) / COUNT(DISTINCT EXTRACT(YEAR FROM a.taskdate)) AS stops_per_active_year,
+       COUNT(DISTINCT l.signature) / COUNT(DISTINCT EXTRACT(YEAR FROM a.taskdate)) AS loans_per_active_year,
+       NVL(
+           TRUNC(100 * COUNT(CASE WHEN l.return IS NULL AND l.signature IS NOT NULL THEN 1 END)
+           / NULLIF(COUNT(l.signature), 0), 3), 0
+       ) || '%' AS percent_unreturned  --We truncate to 3 decimals and add '%'
+FROM drivers d
+JOIN assign_drv a ON d.passport = a.passport
+JOIN stops s ON a.route_id = s.route_id
+JOIN services sv ON a.passport = sv.passport
+LEFT JOIN loans l ON l.town = sv.town AND sv.taskdate = l.stopdate AND sv.passport = a.passport
+GROUP BY d.fullname, d.birthdate, d.cont_start;
+
+INSERT INTO loans (SIGNATURE, USER_ID, STOPDATE, TOWN, PROVINCE, TYPE, TIME, RETURN) 
+VALUES ('PD137', '0546923640', TO_DATE('20-NOV-2024', 'DD-MON-YYYY'), 'Sotoverde de Debajo', 'Castellón', 'L', 120, NULL); 
+
+INSERT INTO loans (SIGNATURE, USER_ID, STOPDATE, TOWN, PROVINCE, TYPE, TIME, RETURN) 
+VALUES ('NB427', '0546923640', TO_DATE('20-NOV-2024', 'DD-MON-YYYY'), 'Sotoverde de Debajo', 'Castellón', 'L', 120, NULL); 
+
+INSERT INTO loans (SIGNATURE, USER_ID, STOPDATE, TOWN, PROVINCE, TYPE, TIME, RETURN) 
+VALUES ('GL007', '0546923640', TO_DATE('20-NOV-2024', 'DD-MON-YYYY'), 'Sotoverde de Debajo', 'Castellón', 'L', 120, NULL); 
+
+
+UPDATE copies  
+SET CONDITION = 'D'  
+WHERE SIGNATURE IN ('CH068', 'IA676', 'AL204'); 
+
+UPDATE copies 
+SET CONDITION = 'D'
+WHERE SIGNATURE = 'CH068';
+
+UPDATE copies 
+SET CONDITION = 'D'
+WHERE SIGNATURE = 'XX777';
+
+
+INSERT INTO loans (SIGNATURE, USER_ID, STOPDATE, TOWN, PROVINCE, TYPE, TIME, RETURN) 
+VALUES ('PD137', '0546923640', TO_DATE('20-NOV-2024', 'DD-MON-YYYY'), 'Sotoverde de Debajo', 'Castellón', 'L', 120, NULL);
+
+INSERT INTO loans (SIGNATURE, USER_ID, STOPDATE, TOWN, PROVINCE, TYPE, TIME, RETURN) 
+VALUES ('XX777', '0546923640', TO_DATE('20-NOV-2024', 'DD-MON-YYYY'), 'Sotoverde de Debajo', 'Castellón', 'L', 120, NULL);
