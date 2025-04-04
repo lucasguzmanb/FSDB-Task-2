e8b28fbb1b6d73c452cf5af26412039d1bceae33
Lucas Guzman <lucas_guzman@hotmail.es>
2025-04-04 10:52:44 +0200

    format

--- triggers_test.sql
+++ triggers_test.sql
@@ -1,42 +1,11 @@
-INSERT INTO users VALUES (
-    'LIB001',
-    'ID123456789012345',
-    'Biblioteca Central',
-    'Apellido1',
-    'Apellido2',
-    DATE '1970-01-01',
-    'Valsolana',
-    'Madrid',
-    'Calle Falsa 123',
-    'lib@biblioteca.com',
-    123456789,
-    'L',
-    NULL
-);
+INSERT INTO users VALUES ('LIB001', 'ID123456789012345', 'Testing library', 'Sur1', 'Sur2', DATE '1970-01-01', 'Valsolana', 'Madrid', 'Fakestreet 123', 'lib@library.com', 123456789, 'L', NULL);
 
 -- Dummy data for loans table
-INSERT INTO loans VALUES (
-    'CK239',          
-    'LIB001',         
-    TRUNC(SYSDATE),   
-    'Valsolana',      
-    'Madrid',         
-    'L',              
-    0,
-    TRUNC(SYSDATE)+14,
-);
+INSERT INTO loans VALUES ('CK239', 'LIB001', TRUNC(SYSDATE), 'Valsolana', 'Madrid', 'L', 0, TRUNC(SYSDATE)+14);
 
 -- Try to insert a post with an institutional user
 BEGIN
-    INSERT INTO posts VALUES (
-        'CK239',
-        'LIB001',
-        TRUNC(SYSDATE), 
-        TRUNC(SYSDATE)+1,
-        'Posting a comment from an institutional user', 
-        0, 
-        0
-    );
+    INSERT INTO posts VALUES ('CK239', 'LIB001', TRUNC(SYSDATE), TRUNC(SYSDATE)+1, 'Posting a comment from an institutional user', 0, 0);
     DBMS_OUTPUT.PUT_LINE('Post inserted successfully (this should not happen).');
 EXCEPTION
     WHEN OTHERS THEN
